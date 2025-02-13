68faa26eca9c327a77dc51befe28868d14771488
Hassan Azmy <hassanazmy13579@gmail.com>
2025-02-14 01:24:29 +0200

    Fix the Last Commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import fs from 'fs';
 
+//* Types of Streams *//
+/*
+   * Readable  Stream => From which data can be read
+   * Writable  Stream => To which we can write data
+   * Dublex    Stream => That are both readable and writable
+   * Transform Stream => that can modify or transform the data as its written or read
+*/
+
 const readeableStream = fs.createReadStream('./file1.txt', {
    encoding: "utf-8",
    highWaterMark: 2
@@ -7,6 +15,7 @@ const readeableStream = fs.createReadStream('./file1.txt', {
 
 const writableStream = fs.createWriteStream('./file2.txt');
 
+//* It extends from EventEmitter
 readeableStream.on('data', chunk => {
    console.log(chunk);
    writableStream.write(chunk);
