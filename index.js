58e53dbb44f9ed9b0340eded34bfdb12d690803d
Hassan Azmy <hassanazmy13579@gmail.com>
2025-02-20 19:53:39 +0200

    HTML Response Using Streams & Pipe

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { log } from 'console';
 import htpp from 'http';
 import fs from 'fs';
+const __dirname = import.meta.dirname;
 
 //* (req, res) => {} is a request listener
 const server = htpp.createServer((req, res) => {
@@ -9,13 +10,11 @@ const server = htpp.createServer((req, res) => {
       lasttName: 'Azmy',
    };
 
-   const html = fs.readFileSync("./index.html", "utf-8");
-
    res.writeHead(200, {
       "content-type": 'text/html'
    });
-   
-   res.end(html);
+
+   fs.createReadStream(__dirname + "/index.html").pipe(res);
 });
 
 server.listen(3000, () => {
