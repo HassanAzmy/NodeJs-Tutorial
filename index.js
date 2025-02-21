657592b66ddd6a5b285597efc2e596024d145303
Hassan Azmy <hassanazmy13579@gmail.com>
2025-02-21 13:19:38 +0200

    Thread Pool Size
    Note that every 4 calls is done together and the other wait for them to be done which indicates that the thread pool size is 4

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import crypto from 'crypto';
 
 //* Since we use the Async version. It's run parallel thus taking the same time
 const start = Date.now();
-const MAX_CALLS = 3;
+const MAX_CALLS = 12;
 for(let i = 0; i < MAX_CALLS; i ++) {
    crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
       console.log(`Hash: ${i + 1}`, Date.now() - start);
    });
-} 
+}
