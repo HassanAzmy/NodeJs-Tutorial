9159a11ccac28e625904e56731843edeabf6facf
Hassan Azmy <hassanazmy13579@gmail.com>
2025-02-21 14:31:32 +0200

    Thread Pool Size
    We are able to increase the size of the thread pool by running the following scipt set UV_THREADPOOL_SIZE=NEW_VAL & node app.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import crypto from 'crypto';
 
-//* Since we use the Async version. It's run parallel thus taking the same time
+// process.env.UV_THREADPOOL_SIZE = 5;
+
 const start = Date.now();
-const MAX_CALLS = 12;
+const MAX_CALLS = 6;
 for(let i = 0; i < MAX_CALLS; i ++) {
    crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
       console.log(`Hash: ${i + 1}`, Date.now() - start);
